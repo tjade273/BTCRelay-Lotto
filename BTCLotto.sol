890a30182e4712e537245e075569dfe4cb1eb189
Tjaden Hess <tjade273@gmail.com>
2016-06-17 11:40:33 -0400

    Add getters

--- BTCLotto.sol
+++ BTCLotto.sol
@@ -3,12 +3,14 @@ import "./RoundLib.sol";
 
 contract BTCLotto{
   using RoundLib for RoundLib.Round;
-  address btcRelay = 0x41f274c0023f83391de4e0733c609df5a124c3d4;
+  address public btcRelay = 0x41f274c0023f83391de4e0733c609df5a124c3d4;
   uint i;
   mapping(uint => RoundLib.Round) rounds;
   uint maxVal;
-  uint constant roundLen = 6;
-  uint constant price = 1 ether;
+  uint constant public roundLen = 6;
+  uint constant public price = 1 ether;
+
+  event BoughtTicket(uint id, address buyer, uint num);
 
   function BTCLotto(address relay, uint max){
     if(relay != 0){
@@ -25,10 +27,15 @@ contract BTCLotto{
 
   function buyTicket(uint roundIndex){
     rounds[roundIndex].buyTickets(btcRelay);
+    BoughtTicket(roundIndex, msg.sender, msg.value/price);
   }
 
   function payOut(uint roundIndex){
     rounds[roundIndex].payOut(btcRelay);
+  }
+
+  function getRound(uint i) constant returns (address[], uint, uint, RoundLib.Phase){
+      return (rounds[i].tickets,rounds[i].value,rounds[i].btcBlockNum,rounds[i].phase);
   }
 
 
